3a7081bc5eff8e2e581c0e21a01960231195a51f
emrullahardic <emrullahardc@gmail.com>
2022-09-09 01:17:30 +0300

    all-project

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,12 +1,20 @@
 import './ExploreContainer.css';
+import {ReadCardByNFC} from "drifter-nfc";
 
 interface ContainerProps { }
+
+
+const startNfc = async () => {
+    ReadCardByNFC.readCard('filter');
+}
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
     <div className="container">
       <strong>Ready to create an app?</strong>
       <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <button onClick={()=>startNfc()} >Nfc Oku</button>
+
     </div>
   );
 };
